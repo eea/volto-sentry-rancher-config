ac9bab781a4a1d48049abe9dd8dfe019e470ca52
Alin Voinea <contact@avoinea.com>
2020-11-16 18:11:13 +0200

    Configure Sentry via sentryOptions config.settings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
+import os from 'os';
+
 const applyConfig = (config) => {
+  const sentryOptions = {
+    environment: 'production',
+    serverName: os.hostname(),
+    logger: 'volto',
+    tags: {
+      site: 'localhost',
+      logger: 'volto',
+      serverName: os.hostname(),
+    },
+  };
+
+  if (config?.settings?.apiPath) {
+    sentryOptions.tags.site = config.settings.apiPath
+      .replace('/api', '')
+      .replace('https://', '')
+      .replace('http://', '');
+  }
+
+  if (sentryOptions.tags.site.includes('localhost')) {
+    sentryOptions.environment = 'development';
+  }
+
+  if (sentryOptions.tags.site.includes('dev')) {
+    sentryOptions.environment = 'development';
+  }
+
+  config.settings.sentryOptions = {
+    ...config.settings.sentryOptions,
+    ...sentryOptions,
+  };
+
   return config;
 };
 
