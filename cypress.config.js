1a26549a1f24dec93cf64f4c399ccef2a928e87c
Miu Razvan <miu.razvan28@gmail.com>
2022-10-24 13:21:06 +0300

    Update  cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // e2e testing node events setup code
+      return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:3000',
   },
